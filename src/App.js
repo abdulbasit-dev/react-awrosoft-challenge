c258d78d3d12a5118f0b8628e9f6dd61b1f369b3
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-07-04 18:50:15 +0300

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import React, {useEffect} from 'react';
+import {db} from './firebase';
+
 function App() {
+  // useEffect =>runs a piece of code base on specific condition
+  // useEffect(() => {
+  //   db.collection('ss').onSnapshot(snapshot => {
+  //     console.log(snapshot.docs.map(doc => ({id: doc.id, post: doc.data()})));
+  //   });
+  // }, []);
+
   return (
     <div>
       <h1>sac</h1>
